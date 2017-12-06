6de26acbdb87e1b920737f75410fe4609b495ed5
erick torres <ericktorres@cis.vvc.edu>
2017-12-05 22:07:40 -0800

    update

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 * File: main.cpp
 * Developer: Erick Torres
 * Email: erickt@rocketmail.com
-* Description: This is the second submission of homework Project P6.13, it was edited to complete results.
+* Description: This is the second submission of homework Project P6.13, after it was re-opened to submit.
 */
 #include<iostream>
 #include<string>
 #include<vector>
+#include<iomanip>
 
 class Transaction
 {
@@ -15,7 +16,7 @@ public:
    	void read();
    	int get_day();
    	double get_amount();
-   	void print() const;
+	std::string get_des();
 private:
    	int day;
    	double amount;
@@ -38,19 +39,16 @@ double Transaction::get_amount()
 	return amount;
 }
 
+std::string Transaction::get_des()
+{
+	return description;
+}
+
 void Transaction::read()
 {
 	std:: cout << "Day || Amount || Description" << std::endl;   
     	std::cin >> day >> amount;
-    	std::cin.ignore();
     	std::getline(std::cin,description);
-}
-
-void Transaction::print() const
-{
-   	std::cout << "day:" << day << std::endl;
-    	std::cout << "amount:" << amount << std::endl;
-    	std::cout << "description:" << description << std::endl;
 }
 
 void print_transactions(const std::vector<Transaction> t)
@@ -62,97 +60,126 @@ void print_transactions(const std::vector<Transaction> t)
 class Balance
 {
 public:
-	Balance();
+	Balance(double);
 	void add_trans(Transaction t); 
- 	void get_balance();
  	void print();
- 	double get_avg();
- 	double get_min();
-	double get_int();
+	void calc_avg(); 
+	void calc_daily();
+	void calc_int();
+	void calc_min();
 private:
 	std::vector<Transaction> trans;
 	std::vector<double> balances;
+	double initial_bal;
 	double balance;
 	double I;
 	double int_total;
+	double avg;
+	double min;
 };
 
-Balance::Balance()
+Balance::Balance(double b)
 {
-	balance = 0.0;
-	double int_total = 0.0;
-	I = 0.005;
+	initial_bal = b;
+	balance = b; 
+	int_total = 0.0;
+	I = 0.005; //given interest
+	avg = 0;
 }
-
 void Balance::add_trans(Transaction t)
 {
 	trans.push_back(t);
 }
 
-void Balance::get_balance()
-{
-	double sum = 0.0;
-	for(int i=0; i<=trans.size(); i++)
-	{
-	sum+= trans[i].get_amount();
-	}
-	balance = sum;
-	get_int();
-}
-
 void Balance::print()
-{
-	std::cout<< "your interest is:" << int_total <<  "\n your balance is" << balance << std::endl;
-} 
+{	
+	double rbalance = initial_bal;
+	std::cout<< "Day || Amount || Description || Balance " << std::endl 
+		 << "========================================" << std::endl
+		 << "   1                                "<< initial_bal << std::endl;
+	for(int i=0; i<trans.size();i++)
+	 {
+	  rbalance += trans[i].get_amount();
+	  std::cout << std::setw(4) << trans[i].get_day() 
+		    << std::setw(10) << trans[i].get_amount()
+		    << std::setw(15) << trans[i].get_des()
+		    << std::setw(11) << rbalance << std::endl;
+	 }
+	std::cout<< "========================================" << std::endl;
 
-double Balance::get_avg()
-{
-	double b_sum = 0.0;
-	for(int day = 0; day < balance.size(); day++)
-	{
-	 b_sum += balance[day];
-	}
+	std::cout<< "Your minimum daily balance is: " << std::setprecision(2)<< std::fixed << std::setw(8) << min << std::endl;
 	
-	return b_sum/
+	std::cout<< "Your daily average balance is: " << std::setw(8) << avg << std::endl;
 }
 
-double Balance::get_min()
+//this function finds the average by adding all balances stored and dividing by total inputed.
+void Balance::calc_avg()
 {
+	double bb = 0;
+	for(int i=0;i<balances.size();i++)
+	{
+	 bb += balances[i];
+	}
+	avg= bb/balances.size();
+
 }
 
-double Balance::get_int()
+//this function calculates and stores balances
+void Balance::calc_daily()
 {
-	int prv = trans[trans.size()-2].get_day() / 30;
-	int current = trans[trans.size()-1].get_day() / 30;
-
-	if(current > prv) 
+	for(int i=1;i<=30;i++)
 	{
-	balance = (balance * I) + balance;
-	int_total += I * balance;
+	 for(int j=0; j<trans.size();j++)
+	 {
+	  if(trans[j].get_day() == i)
+	  {
+	   balance += trans[j].get_amount();
+	  }
+	 }
+	balances.push_back(balance);
 	}
+}
 
-	return int_total;
 
-	
+void Balance::calc_int()
+{
+	 int_total = balance * I;
+	 balance += int_total;
+ 	 balances[29] = balance;
+}
+
+//this function finds the smallest balance in vector
+void Balance::calc_min()
+{
+	min = balance;
+	for(int i=0;i<balances.size();i++)
+	{
+	 if(balances[i]< min)
+	 {
+	  min = balances[i];
+	 }
+	}	
+
 }
 
 int main()
 {
-	Balance b;
+	Balance b(1000);
  	bool more = true;
   	while(more)
    	{
        	 Transaction t;
        	 t.read();
       	 b.add_trans(t);
-	 b.get_balance();
-	// b.get_int();
 	 std::cout << "Enter another (Y/N)";
          char response;
       	 std::cin >> response;
       	 if(response != 'y' && response != 'Y')
     	 more = false;	
    	}
+	b.calc_daily();
+	b.calc_avg();
+	b.calc_min();
 	b.print();
  return 0;
 }
